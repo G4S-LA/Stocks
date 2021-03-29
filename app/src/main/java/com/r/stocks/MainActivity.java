e6aaa1f9467c8d2e52e826f38f1789573d03e09f
G4S-LA <labworkspace@yandex.ru>
2021-03-29 13:32:13 +0300

    Removing unnecessary dependencies

--- app/src/main/java/com/r/stocks/MainActivity.java
+++ app/src/main/java/com/r/stocks/MainActivity.java
@@ -22,14 +22,7 @@ import com.r.stocks.utils.Status;
 import com.r.stocks.models.CompanyModel;
 import com.r.stocks.viewmodels.StocksViewModel;
 
-
-import java.io.File;
-import java.io.FileOutputStream;
 import java.util.List;
-
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-
 
 public class MainActivity extends AppCompatActivity implements StocksAdapter.OnStocksListener {
 
